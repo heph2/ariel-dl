963aac9e31452b9663dc2ee17a8ab46888075fd2
Heph <srht@mrkeebs.eu>
2021-09-26 18:57:59 +0200

    Removed fork support and start TUI support

--- ariel.pl
+++ ariel.pl
@@ -16,6 +16,10 @@ use Mojo::DOM58;
 use HTTP::Cookies;
 use v5.10.0;
 
+## testing TUI
+use Curses::UI;
+#use Curses::UI::Grid;
+
 my $username;
 my $password;
 my @credentials;
@@ -96,17 +100,17 @@ sub fetch_link {
 my @links = fetch_link( $username, $password );
 
 ## now we print a table with index and all the link
-my $index = 1;
-my %l;
-for (@links) {
-    $l{$index} = $_;
-    print "$index  | $_\n";
-    $index++;
-}
+# my $index = 1;
+# my %l;
+# for (@links) {
+#     $l{$index} = $_;
+#     print "$index  | $_\n";
+#     $index++;
+# }
 
-print "Choose video to download: ";
-my $download_link = <STDIN>;
-chomp $download_link;
+# print "Choose video to download: ";
+# my $download_link = <STDIN>;
+# chomp $download_link;
 
 ## Add to an array all the links that has to be downloaded
 ## There are 3 types of accepted Index
@@ -114,82 +118,167 @@ chomp $download_link;
 ## 1
 ## 1-4  (Download from 1 to 4)
 ## 1,4 (Download 1 and 4)
-my @to_down;
-for (keys %l) {
-    if ($download_link =~ /\d-\d/) {  ## range
-        my ($from) = $download_link =~ /(\d)-/;
-        my ($to) = $download_link =~ /-(\d)/;
-        my $index = $from;
-        for ($from..$to) {
-            push(@to_down, $l{$index});
-            $index++;
+# my @to_down;
+# if ($download_link =~ /\d-\d/) {  ## range
+#     my ($from) = $download_link =~ /(\d)-/;
+#     my ($to) = $download_link =~ /-(\d)/;
+#     my $index = $from;
+#     for ($from..$to) {
+#         push(@to_down, $l{$index});
+#         $index++;
+#     }
+# }
+
+# if ($download_link =~ /,+/) {  ## Comma-separated
+#     my @x = split(/,/, $download_link);
+#     for (@x) {
+#         push(@to_down, $l{$_});
+#     }
+# }
+
+# if ($download_link =~ /\d{1}/) {  ## Single
+#     push(@to_down, $l{$download_link});
+# }
+
+# chdir $down_dir; # change directory to down
+
+# for (@to_down) {
+#     ## Output use a regexp for extract the name of the file
+#     ## from the URLs
+#     my ($output) = $_ =~ /mp4:(.*\.mp4)/;
+
+#     system("ffmpeg -loglevel panic -i $_ -c:v copy -c:a copy -crf 50 $output");
+#     say "DONE!";
+# }
+
+
+## TESTING TUI
+my $debug = 0;
+
+my $cui = new Curses::UI (
+    -color_support => 1,
+    -clear_on_exit => 1,
+    -debug => $debug,
+    );
+
+create_promote_deps_window();
+$cui->set_binding( \&exit_dialog , "\cQ");
+$cui->mainloop();
+
+sub exit_dialog {
+    my $return = $cui->dialog(
+        -message   => "Do you really want to quit?",
+        -title     => "Confirm",
+        -buttons   => ['yes', 'no'],
+    );
+
+    exit(0) if $return;
+}
+
+sub create_base_window {
+    my ($name) = @_;
+
+    $cui->add(
+        $name,
+        'Window', 
+        -border       => 1, 
+        -titlereverse => 0, 
+        -padtop       => 2, 
+        -padbottom    => 3, 
+        -ipad         => 1,
+        -title        => 'CTRL-Q to quiz',
+    );
+}
+
+sub move_focus {
+    my $widget = $_[0];
+    my $key    = $_[1];
+
+    if ($key eq "\t") {
+        $widget->parent()->focus_next();
+    }
+    else {
+        $widget->parent()->focus_prev();
+    }
+}
+
+sub create_promote_deps_window {
+    my ($name) = @_;
+
+    my $win = create_base_window($name);
+
+    my $grid = $win->add(
+        'grid',
+        'Grid',
+        -height       => 14,
+        -width        => -1,
+        -editable     => 0,
+        -border       => 1,
+        -process_bindings => {
+            CUI_TAB => undef,
+        },
+        # -bg       => "blue",
+        # -fg       => "white",
+    );
+
+    $grid->add_cell(
+        "Index",
+        -width => 10,
+        -label => "INDEX"
+        );
+    
+    $grid->add_cell(
+        "Link",
+        -width => 10,
+        -label => "Link"
+        );
+
+    my $button_callback = sub {
+        my $this = shift;
+        
+        my $btn_name = $this->get();
+        if ($btn_name eq "Back") {
+            # give up promotion and return to promote window
+            $win->focus();
         }
-    }
-
-    if ($download_link =~ /\d(,\d)*/) {  ## Comma-separated
-        my @x = split(/,/, $download_link);
-        for (@x) {
-            push(@to_down, $l{$_});
+        elsif ($btn_name eq "PromoteWithDeps") {
         }
+    };
+    $win->add(
+        undef,
+        'Buttonbox',
+        -y        => -1,
+        -buttons  => [
+            {
+                -label   => "< Back >",
+                -value   => "Back",
+                -onpress => $button_callback,
+            },
+            {
+                -label   => "< Promote w/ all deps >",
+                -value   => "PromoteWithDeps",
+                -onpress => $button_callback,
+            },
+        ],
+    );
+    
+    foreach my $f (@links) {
+
+        $grid->add_row(
+            undef,
+            # -fg    => 'black',
+            # -bg    => 'yellow',
+            -cells => {
+                otp     => $f->[0],
+                commit1 => $f->[1],
+                otnp    => $f->[2],
+                commit2 => $f->[3],
+                overlap => $f->[4],
+            }
+        );
     }
 
-    if ($download_link =~ /\d{1}/) {  ## Single
-        push(@to_down, $l{$download_link});
-    }
+    $grid->layout();
+    return $win;
 }
 
-## Adding signal handler for INT( Ctrl-C on the keyboard ), and
-## for TERM, that cause Perl to exit cleanly
-## If fork fails, parent send a TERM to all child process and then exit
-$SIG{INT} = $SIG{TERM} = sub { exit };
-
-my $parent_pid = "$$";
-## now use the links for execute ffmpeg and download, it will spawn a
-## process for each link and download them "concurrently"
-my @children;
-
-chdir $down_dir; # change directory to down
-#for my $link (@links) {
-for (@to_down) {
-    my $pid = fork;
-    if (!defined $pid) {
-        warn "failed to fork: $!";
-        kill 'TERM', @children;
-        exit;
-    }
-    elsif ($pid) {
-        push @children, $pid;
-        next;
-    }
-
-    ## Output use a regexp for extract the name of the file
-    ## from the URLs
-    my ($output) = $_ =~ /mp4:(.*\.mp4)/;
-    say "Downloading $output on process $pid";
-
-    ## Actually call fffmpeg and download the m3u8 file
-    #    system("ffmpeg -loglevel panic -i $link -c:v copy -c:a copy -crf 50 $output");
-    system("ffmpeg -loglevel panic -i $_ -c:v copy -c:a copy -crf 50 $output");
-    say "DONE!";
-
-    exit;
-}
-
-## wait_children performs blocking wait call on the pids forked by the parent
-wait_children();
-
-sub wait_children {
-    while (scalar @children) {
-        my $pid = $children[0];
-        my $kid = waitpid $pid, 0;
-        warn "Reaped $pid ($kid)\n";
-        shift @children;
-    }
-}
-
-## Additional clean up if some zombie process spawned earlier exists
-END {
-    if ($parent_pid == $$) {
-        wait_children();
-    }
-}
