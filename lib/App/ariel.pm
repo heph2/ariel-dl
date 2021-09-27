db3f83c928eb5b4fc03addbb6805bed3fd2a3cc6
Heph <srht@mrkeebs.eu>
2021-09-27 19:23:10 +0200

    Add Makefile and app module

---
+++ lib/App/ariel.pm
@@ -0,0 +1,23 @@
+package App::Ariel;
+
+our $VERSION = 1.0;
+
+=head1 NAME
+
+App::Ariel - a simple scraper and downloader for ariel
+
+=head1 DESCRIPTION
+
+This is a stub module, see F<./atlas> for details of the app.
+
+=head1 AUTHOR
+
+Heph
+
+=head1 LICENSE
+
+GNU General Public License
+
+=cut
+
+1;
